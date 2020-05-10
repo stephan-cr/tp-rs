575d2e32788661a1c3666f3458b98311c9cdb688
Stephan Creutz <stephan.cr@gmx.de>
2020-05-10 10:03:24 +0200

    integrate reset in integration test as well

--- tests/int_test.rs
+++ tests/int_test.rs
@@ -9,7 +9,9 @@ use std::time::Instant;
 fn it_works() {
     let mut tp: Throughput<Instant> = Throughput::new();
     tp.report(1);
+    tp.reset();
 
     let tp: Arc<ThroughputSynchronized<Instant>> = Arc::new(ThroughputSynchronized::new());
     tp.report(1);
+    tp.reset();
 }
